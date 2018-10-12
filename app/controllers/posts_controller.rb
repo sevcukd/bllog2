404133e2c22b24410a4a1b0767d38d035da41e20
sevcukd <sevcukd9@gmail.com>
2018-10-12 13:47:58 +0300

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+   def index
+  end
+    def new
+    	@post =Post.new
+  end
+    def create
+    	@post = Post.new(post_params)
+    	if @post.save
+    		redirect_to root_path
+    		
+    	end
+    end
+    
+    	def post_params
+    		params.require(:post).permit(:username, :title, :body)
+    		
+    	end
+end
